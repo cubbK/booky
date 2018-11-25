681c05bd972652d84f4281963ce0cb6a494e012b
Dan <danpopa1998@gmail.com>
2018-11-25 17:41:39 +0200

    bootstrap nestjs

--- server/helpers/getGroupNameFromLink.js
+++
@@ -1,9 +0,0 @@
-var URL = require("url-parse");
-
-function getGroupNameFromLink(link) {
-  const url = new URL(link);
-  const hostnameWithoutWWW = url.hostname.replace(/^www./, "");
-  return hostnameWithoutWWW;
-}
-
-module.exports = getGroupNameFromLink;
