edf119c583cb3587919549ed9d227a37e92aba85
Dan <danpopa1998@gmail.com>
2018-12-04 22:58:20 +0200

    add role to addLink

--- server/src/links/links.controller.ts
+++ server/src/links/links.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UseGuards, Body, Req, Put } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body, Req, Put, Delete, Param } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserIdFromJwt } from 'src/users/userIdFromJwt.decorator';
 import { LinkDto } from './link.dto';
@@ -12,21 +12,36 @@ export class LinksController {
   constructor(private readonly linksService: LinksService) {}
 
   @Post('add')
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(AuthGuard('jwt'), ACGuard)
+  @UseRoles({
+		resource:  'link',
+		action:  'create',
+		possession:  'own',
+	})
   async addLink(@UserIdFromJwt() userId, @Body() link: LinkDto) {
     return this.linksService.addLink(link, userId);
   }
 
-  @Put('favorite')
+  @Put('favorite/:linkId/:toFavorite')
   @UseGuards(AuthGuard('jwt'), ACGuard)
   @UseRoles({
 		resource:  'link',
 		action:  'update',
 		possession:  'own',
 	})
-  async favoriteLink(@Body() payload: ToFavoriteDto) {
+  async favoriteLink(@Param('linkId') linkId: number, @Param('toFavorite') toFavorite: boolean) {
 
-    return this.linksService.favoriteLink(payload.toFavorite, payload.linkId);
+    return this.linksService.favoriteLink(toFavorite, linkId);
   }
 
+  @Delete('delete/:linkId')
+  @UseGuards(AuthGuard('jwt'), ACGuard)
+  @UseRoles({
+		resource:  'link',
+		action:  'delete',
+		possession:  'own',
+  })
+  async deleteLink(@Param('linkId') linkId: number) {
+    console.log('delete' + linkId);
+  }
 }
