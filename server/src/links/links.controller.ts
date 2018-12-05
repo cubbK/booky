1964bcc395fc8c5d6f431d88a6add9f52c83b258
Dan <danpopa1998@gmail.com>
2018-12-05 14:34:30 +0200

    start writing decorator for detecting the ownership

--- server/src/links/links.controller.ts
+++ server/src/links/links.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Post, UseGuards, Body, Req, Put, Delete, Param } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Body,
+  Req,
+  Put,
+  Delete,
+  Param,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserIdFromJwt } from 'src/users/userIdFromJwt.decorator';
 import { LinkDto } from './link.dto';
@@ -6,41 +15,33 @@ import { LinksService } from './links.service';
 import { ToFavoriteDto } from './toFavorite.dto';
 import { ACGuard, UseRoles } from 'nest-access-control';
 import { User } from 'src/users/user.entity';
+import { UserBelongsToResourceGuard } from 'src/UseBelongsToResource.guard';
 
 @Controller('links')
 export class LinksController {
   constructor(private readonly linksService: LinksService) {}
 
   @Post('add')
-  @UseGuards(AuthGuard('jwt'), ACGuard)
-  @UseRoles({
-		resource:  'link',
-		action:  'create',
-		possession:  'own',
-	})
+  @UseGuards(AuthGuard('jwt'))
   async addLink(@UserIdFromJwt() userId, @Body() link: LinkDto) {
     return this.linksService.addLink(link, userId);
   }
 
   @Put('favorite/:linkId/:toFavorite')
-  @UseGuards(AuthGuard('jwt'), ACGuard)
-  @UseRoles({
-		resource:  'link',
-		action:  'update',
-		possession:  'own',
-	})
-  async favoriteLink(@Param('linkId') linkId: number, @Param('toFavorite') toFavorite: boolean) {
-
+  @UseGuards(AuthGuard('jwt'), new UserBelongsToResourceGuard({
+    itemIdName: 'linkId',
+    type: 'Param',
+    itemsFieldName: 'links',
+  }))
+  async favoriteLink(
+    @Param('linkId') linkId: number,
+    @Param('toFavorite') toFavorite: boolean,
+  ) {
     return this.linksService.favoriteLink(toFavorite, linkId);
   }
 
   @Delete('delete/:linkId')
-  @UseGuards(AuthGuard('jwt'), ACGuard)
-  @UseRoles({
-		resource:  'link',
-		action:  'delete',
-		possession:  'own',
-  })
+  @UseGuards(AuthGuard('jwt'))
   async deleteLink(@Param('linkId') linkId: number) {
     console.log('delete' + linkId);
   }
