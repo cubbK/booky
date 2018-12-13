1517507fcb4a889e59bd7922c35740c0c99900fb
Dan <danpopa1998@gmail.com>
2018-12-13 22:46:53 +0200

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { frontendUrl } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: `${frontendUrl}`,
+    });
   await app.listen(4001);
 }
 bootstrap();
