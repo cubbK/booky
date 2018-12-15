bade43dc6f785f44114ce514dcd56ee67267cf3b
Dan <danpopa1998@gmail.com>
2018-12-15 15:22:27 +0200

    add refresh case to jwtReducer

--- client/src/redux/reducers/jwtReducer.ts
+++ client/src/redux/reducers/jwtReducer.ts
@@ -1,4 +1,4 @@
-import { SET_JWT } from "../actionTypes";
+import { SET_JWT, REFRESH_JWT } from "../actionTypes";
 
 const defaultState: null | string = null;
 
@@ -6,6 +6,8 @@ export function jwtReducer(state = defaultState, action: any) {
   switch (action.type) {
     case SET_JWT:
       return action.payload;
+    case REFRESH_JWT + "_FULFILLED":
+      return action.payload.data;
     default:
       return state;
   }
